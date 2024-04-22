c23836b9b4a33a1937c2dff34629e6911c788f23
AbdelrahmanElhosiny <abdohosini13@gmail.com>
2024-04-22 09:53:24 +0200

    add fonts

---
+++ src/styles/fonts.ts
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+const fonts = {
+  NavText: css`
+    font-family: "nav-text";
+  `,
+  CenterText: css`
+    font-family: "center-text";
+  `,
+  RotatingText: css`
+    font-family: "rotating-text";
+  `,
+  SecondaryText: css`
+    font-family: "secondary-text";
+  `,
+};
+
+export default fonts;
